e491810d8e2fb5dfc77b3915865664ef94d45e1f
Andrei Gog <andrei.gog@pearson.com>
2022-01-26 17:52:23 +0200

    fix for deserialization

--- planet/src/main/java/com/andreigog/planet/transfer/FetchPlanetsRequest.java
+++ planet/src/main/java/com/andreigog/planet/transfer/FetchPlanetsRequest.java
@@ -4,9 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @Getter
+@Setter
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
